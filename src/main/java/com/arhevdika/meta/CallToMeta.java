e7aeeb633f64fec74d3c474bd5729f0433b619fb
rksharma2109 <rksharma21@hotmail.com>
2023-10-13 22:50:52 +0530

    Add files via upload

--- src/main/java/com/arhevdika/meta/CallToMeta.java
+++ src/main/java/com/arhevdika/meta/CallToMeta.java
@@ -25,7 +25,7 @@ import org.springframework.web.client.HttpServerErrorException.InternalServerErr
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.arhevdika.meta.Encdec;
-@CrossOrigin(origins = "https://websitetometa.blob.core.windows.net/staticwebsite")
+@CrossOrigin(origins = "*")
 @RestController
 public class CallToMeta {
 
