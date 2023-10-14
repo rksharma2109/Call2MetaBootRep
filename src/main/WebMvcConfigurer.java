75f8666d5cdfd172ee465d5b64b790b234d36a4f
rksharma2109 <rksharma21@hotmail.com>
2023-10-14 21:05:29 +0530

    Add files via upload

---
+++ src/main/WebMvcConfigurer.java
@@ -0,0 +1,13 @@
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://websitetometa.blob.core.windows.net") // Add your website's domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
