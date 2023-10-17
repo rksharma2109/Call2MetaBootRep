1f200e3702d85f28ba852a721d53f47dfaf71c9a
rksharma2109 <rksharma21@hotmail.com>
2023-10-17 22:53:55 +0530

    Update CallToMeta.java

--- src/main/java/com/arhevdika/meta/CallToMeta.java
+++ src/main/java/com/arhevdika/meta/CallToMeta.java
@@ -41,6 +41,10 @@ public class CallToMeta {
     public String inbound(){
         return "Inbound TLS is working!!";
     }
+	 @GetMapping("/index")
+    public String serveIndexPage() {
+        return "index";
+    }
 	@RequestMapping(value={"/meta"}, method = RequestMethod.POST)
 	  public  ResponseEntity<GeneralResponse>  callTometa(@RequestBody MetaPayload metaPayload,HttpServletResponse response )
 			  throws IOException   {
