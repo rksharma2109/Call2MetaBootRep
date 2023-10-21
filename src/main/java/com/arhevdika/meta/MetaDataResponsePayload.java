6d38998c9e05219f6fb5e804043d9432d20ce600
rksharma2109 <rksharma21@hotmail.com>
2023-10-21 19:40:05 +0530

    Add files via upload

---
+++ src/main/java/com/arhevdika/meta/MetaDataResponsePayload.java
@@ -0,0 +1,28 @@
+package com.arhevdika.meta.payload;
+
+
+public class MetaDataResponsePayload {
+String applicant_id;
+String Response;
+String user_pin;
+public String getApplicant_id() {
+	return applicant_id;
+}
+public void setApplicant_id(String applicant_id) {
+	this.applicant_id = applicant_id;
+}
+public String getResponse() {
+	return Response;
+}
+public void setResponse(String response) {
+	Response = response;
+}
+public String getUser_pin() {
+	return user_pin;
+}
+public void setUser_pin(String user_pin) {
+	this.user_pin = user_pin;
+}
+
+}
+
