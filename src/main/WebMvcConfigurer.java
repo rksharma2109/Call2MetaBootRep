2d55fd1c9d69947023c8d552c31127ec8b25bced
rksharma2109 <rksharma21@hotmail.com>
2023-11-16 19:47:57 +0530

    Delete src/main/WebMvcConfigurer.java

--- src/main/WebMvcConfigurer.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-public class WebConfig {
-}
