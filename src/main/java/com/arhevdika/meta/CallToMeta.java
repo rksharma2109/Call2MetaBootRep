b4cfdfdc9b449f996a82efe028ec993de5eb304a
rksharma2109 <rksharma21@hotmail.com>
2023-11-24 10:47:28 +0530

    Update CallToMeta.java

--- src/main/java/com/arhevdika/meta/CallToMeta.java
+++ src/main/java/com/arhevdika/meta/CallToMeta.java
@@ -89,6 +89,7 @@ public  RedirectView gotoNextPage(@RequestBody MetaPayload metaPayload) throws I
 	@PostMapping(value = "/redirectURL")
 	 public String  redirectURL(@RequestBody MetaPayload
 			metaPayload) {
+		 String data="";
 		 try {
 		 System.out.println("in call to meta dukh bhanjan1 api new one");
 		String data="name="+metaPayload.getName()+"&clanguage="+metaPayload.getClanguage()+"&accnum="+metaPayload.getAccnum()+"&loanamout="+metaPayload.getLoanamout()+"&pendingamount="+metaPayload.getPendingamount()+"&EMIamount="+metaPayload.getEMIamount()+"&duedate="+metaPayload.getDuedate()+"&pemi="+metaPayload.getPemi()+"&cinstatus="+metaPayload.getCInstatus()+"&Lastpaiddate="+metaPayload.getLastpaiddate()+"&nextemidate="+metaPayload.getNextemidate()+"&Totalpaidemi="+metaPayload.getTotalpaidemi()+"&Outstanding="+metaPayload.getOutstanding()+"&Preclosureamt="+metaPayload.getPreclosureamt()+"&Centermangcontactno="+metaPayload.getCentermangcontactno()+"&Branchmgrname="+metaPayload.getBranchmgrname()+"&Branchmgrcontactno="+metaPayload.getBranchmgrcontactno(); 
