91afe2206dbb260455d9abcdb2315f3a419ea25d
rksharma2109 <rksharma21@hotmail.com>
2024-02-19 17:47:41 +0530

    Add files via upload

---
+++ src/main/java/com/arhevdika/meta/payload/DataPayload.java
@@ -0,0 +1,13 @@
+package com.arhevdika.meta.payload;
+
+public class DataPayload {
+String Data;
+
+public String getData() {
+	return Data;
+}
+
+public void setData(String data) {
+	Data = data;
+}
+}
