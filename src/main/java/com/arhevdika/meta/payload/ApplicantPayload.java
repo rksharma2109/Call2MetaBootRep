a56d2288a0f333fdb4ff6bfed79203458e1eae62
rksharma2109 <rksharma21@hotmail.com>
2024-02-19 19:01:38 +0530

    Update ApplicantPayload.java

--- src/main/java/com/arhevdika/meta/payload/ApplicantPayload.java
+++ src/main/java/com/arhevdika/meta/payload/ApplicantPayload.java
@@ -2,6 +2,16 @@ package com.arhevdika.meta.payload;
 
 public class ApplicantPayload {
 String Applicant_id;
+String User_pin;
+
+
+public String getUser_pin() {
+	return User_pin;
+}
+
+public void setUser_pin(String user_pin) {
+	User_pin = user_pin;
+}
 
 public String getApplicant_id() {
 	return Applicant_id;
